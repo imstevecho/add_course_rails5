ec479f0e9968b3dcad018367310287cb962daecd
Steve Cho <imstevecho@yahoo.com>
2017-04-02 22:32:51 -0400

    handle exception for article not found

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,19 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  protected
+
+    def resource_not_found
+      message = "The article you are looking for could not be found"
+      flash[:alert] = message
+      redirect_to root_path
+    end
+      
+
   private
 
   def article_params
--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -1,6 +1,5 @@
 require "rails_helper"
 
-
 RSpec.feature "Creating Artiles" do
   
   before do
